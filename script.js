7bbd044fec011fd12e5530793394600e2e2a4266
Hussien Essam <112499614+flavasava2022@users.noreply.github.com>
2023-08-27 05:15:01 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,10 +5,13 @@ const checkBox = document.getElementById("terms");
 checkBox.addEventListener("change", (e) => {
   if (checkBox.checked) {
     popUp.style.display = "block";
+    document.body.style.overflow = "hidden";
+    let hieght = window.scrollY + 80;
+    document.querySelector(".terms").style.top = `${hieght}px`;
   }
 });
 
 document.querySelector(".hide").addEventListener("click", (e) => {
   popUp.style.display = "none";
-  console.log("true");
+  document.body.style.overflow = "unset";
 });
