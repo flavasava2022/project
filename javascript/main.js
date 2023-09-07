44266d6fc9fc0f04f71ee0d593806cd961de49ad
Hussien Essam <112499614+flavasava2022@users.noreply.github.com>
2023-09-07 06:40:56 +0300

    Add files via upload

--- javascript/main.js
+++ javascript/main.js
@@ -1,16 +1,3 @@
-// Homepage Map java
-const map = L.map("map").setView([29.3440892486483, 47.949007143370196], 13);
-
-L.tileLayer("http://{s}.google.com/vt?lyrs=m&x={x}&y={y}&z={z}", {
-  maxZoom: 20,
-  subdomains: ["mt0", "mt1", "mt2", "mt3"],
-}).addTo(map);
-
-L.marker([29.3440892486483, 47.949007143370196])
-  .addTo(map)
-  .bindPopup("مركز كونا لتطوير القدرات الإعلامية")
-  .openPopup();
-
 // popUp Signup Java
 const popUp = document.querySelector(".overlay");
 const checkBox = document.getElementById("terms");
