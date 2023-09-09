5d89fa331c5c9fa5765554b3fe7f12723df47752
Hussien Essam <112499614+flavasava2022@users.noreply.github.com>
2023-09-10 00:25:54 +0300

    Add files via upload

--- javascript/overlay.js
+++ javascript/overlay.js
@@ -5,14 +5,10 @@ const checkBox = document.getElementById("terms");
 checkBox.addEventListener("change", (e) => {
   if (checkBox.checked) {
     popUp.style.display = "block";
-    document.body.style.overflow = "hidden";
-    let hieght = window.scrollY;
-
     document.querySelector(".overlay").style.top = `${hieght}px`;
   }
 });
 
 document.querySelector(".hide").addEventListener("click", (e) => {
   popUp.style.display = "none";
-  document.body.style.overflow = "unset";
 });
