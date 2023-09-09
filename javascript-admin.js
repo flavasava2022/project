a8cc9a00fa82f763185a61203400de7a77e8afb5
Hussien Essam <112499614+flavasava2022@users.noreply.github.com>
2023-09-09 19:58:35 +0300

    Add files via upload

--- javascript-admin.js
+++ javascript-admin.js
@@ -1,16 +1,22 @@
 const aside = document.querySelector("aside");
+const navArrow = document.querySelector(".nav-arrow");
 
 aside.addEventListener("click", (e) => {
-  //   console.log("gggg");
-  //   console.log(e.target.closest());
-  console.log(e.target.closest(".choice"));
   if (e.target.closest(".choice")) {
     document.querySelectorAll(".choice").forEach((item) => {
       item.classList.remove("choosed");
     });
+    document.querySelector(".sub-nav").classList.remove("opened");
+    if (e.target.closest(".choice").classList.contains("sub")) {
+      document.querySelector(".sub-nav").classList.add("opened");
+    }
+
     e.target.closest(".choice").classList.add("choosed");
   }
-  if (e.target.closest(".sub")) {
-    document.querySelector(".sub-nav").classList.toggle("opened");
-  }
+});
+navArrow.addEventListener("click", () => {
+  aside.classList.toggle("aside-display");
+});
+document.querySelector(".right-arrow-nav").addEventListener("click", () => {
+  aside.classList.toggle("aside-display");
 });
